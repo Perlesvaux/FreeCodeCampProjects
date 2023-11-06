00bb0e5d8a3e12f5ca60f7fa2039be8e5ea49dd6
chico <paltryhumanity@gmail.com>
2023-11-05 22:27:36 -0600

    almost

---
+++ Python/probability-calculator/prob_calculator.py
@@ -0,0 +1,73 @@
+import copy
+import random
+# Consider using the modules imported above.
+
+class Hat:
+    def __init__(self, **args):
+        self.keys=args
+        self.contents = []
+        for x in args:
+            for i in range(args[x]):
+                self.contents.append(x)
+
+        self.reserve = copy.copy(self.contents)
+
+    # def draw(self, items_drawn):
+    #     result = []
+    #     pseudo_hat = copy.copy(self.contents)
+    #     # random.shuffle(pseudo_hat)
+    #     if items_drawn > len(pseudo_hat): items_drawn=len(pseudo_hat)
+    #     for x in range(items_drawn):
+    #         selection = random.choice(pseudo_hat)
+    #         result.append(selection)
+    #         pseudo_hat.remove(selection)
+    #     return result
+
+    def draw(self, items_drawn):
+        result = []
+        if items_drawn > len(self.contents) or len(self.contents)==0: return self.reserve
+        pseudo_hat = self.contents
+        # if items_drawn > len(pseudo_hat): items_drawn=len(pseudo_hat)
+        for x in range(items_drawn):
+            selection = random.choice(pseudo_hat)
+            result.append(selection)
+            pseudo_hat.remove(selection)
+        return result
+
+    def __len__(self):
+        return len(self.keys)
+
+
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    expected = []
+    for x in expected_balls:
+        for i in range(expected_balls[x]):
+            expected.append(x)
+    # print(expected)
+    # print(hat.reserve)
+
+    expected_hand=set(expected)
+    success = 0
+    for x in range(num_experiments):
+        pseudo_hat=copy.deepcopy(hat)
+        drawn = pseudo_hat.draw(num_balls_drawn)
+        hand = set(drawn)
+        # hand = set(hat.draw(num_balls_drawn))
+        # print(f"{hand} should be {expected_hand}")
+        print(f"{hand} -> {expected_hand} | {drawn}")
+        if  expected_hand.issubset(hand):
+            success+=1
+
+
+    print(f"{success} -> {num_experiments}")
+    print(len(hat.contents))
+    # return ( num_experiments/success )
+    return success/(num_experiments*len(hat))
+
+
+
+
+
+
---
+++ Python/probability-calculator/testing.py
@@ -0,0 +1,26 @@
+from prob_calculator import Hat
+from prob_calculator import experiment
+# h = Hat(green=5, blue=2)
+# print(h.contents)
+# print(h.draw(2))
+# print(h.draw(2))
+# print(h.draw(2))
+# print(h.draw(2))
+# print(h.draw(2))
+# print(h.draw(2))
+# print(h.draw(2))
+# print(h.draw(2))
+
+# hat = Hat(black=6, red=4, green=3)
+# probability = experiment(hat=hat,
+#                   expected_balls={"red":2,"green":1},
+#                   num_balls_drawn=5,
+#                   num_experiments=5)
+
+
+hat = Hat(black=5, red=5)
+probability = experiment(hat=hat,
+                         expected_balls={"red":1, "black":1},
+                  num_balls_drawn=2,
+                  num_experiments=1000)
+print(probability)
