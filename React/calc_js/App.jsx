dc581b47abb7ae98070d4ed410a0cef81899cce7
chico <paltryhumanity@gmail.com>
2023-11-22 23:05:14 -0600

    Improved layout!

--- React/calc_js/App.jsx
+++ React/calc_js/App.jsx
@@ -118,45 +118,64 @@ function AC(event){
 
 
 return (<>
+<div id="main" className="container-fluid">
 
 
-    <div className="container-fluid">
+    <div className="row"> 
+    <div id="display" className="card">{currentValue? currentValue: "0"} </div>
+    </div>
+    <div className="row">
 
-<div className="row"> 
-<div id="display" className="card">{currentValue? currentValue: "0"} </div>
+    <div className="col-md col-xs col-sm col-lg col-xl">
+
+    <div className="row"> 
+    <button className="btn btn-success   col-md col-xs col-sm col-lg col-xl" id="equals" onClick={equals}>=</button>
+    </div>
+
+    <div className="row"> 
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="seven" >7</button>
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="eight" >8</button>
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="nine"  >9</button>
+    </div>
+
+    <div className="row"> 
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="four"  >4</button>
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="five"  >5</button>
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="six"   >6</button>
+    </div>
+
+    <div className="row"> 
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="one"   >1</button>
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="two"   >2</button>
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="three" >3</button>
+    </div>
+
+    <div className="row"> 
+    <button className="btn btn-primary   col-md-6 col-xs-6 col-sm-6 col-lg-6 col-xl-6" onClick={show} id="zero"  >0</button>
+    <button className="btn btn-primary   col-md col-xs col-sm col-lg col-xl" onClick={show} id="decimal"    >.</button>
+    </div>
+    </div>
+
+    <div className="col-md-4 col-xs-4 col-sm-4 col-lg-4 col-xl-4">
+
+    <div className="row"> 
+    <button className="btn btn-danger    col-md col-xs col-sm col-lg col-xl" onClick={AC} id="clear"    >AC </button>
+    </div>
+    <div className="row"> 
+    <button className="btn btn-secondary col-md col-xs col-sm col-lg col-xl" onClick={show} id="add"       >+</button>
+    </div>
+    <div className="row"> 
+    <button className="btn btn-secondary col-md col-xs col-sm col-lg col-xl" onClick={show} id="divide"    >/</button>
+    </div>
+    <div className="row"> 
+    <button className="btn btn-secondary col-md col-xs col-sm col-lg col-xl" onClick={show} id="multiply"  >x</button>
+    </div>
+    <div className="row"> 
+    <button className="btn btn-secondary col-md col-xs col-sm col-lg col-xl" onClick={show} id="subtract"  >-</button>
+    </div>
+    </div>
 </div>
 
-<div className="row"> 
-<button className="btn btn-success   col-md-6 col-xs-6 col-sm-6 col-lg-6 col-xl-6" id="equals" onClick={equals}>=</button>
-<button className="btn btn-danger    col-md-6 col-xs-6 col-sm-6 col-lg-6 col-xl-6" onClick={AC} id="clear"    >AC </button>
-</div>
-
-<div className="row"> 
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="seven" >7</button>
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="eight" >8</button>
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="nine"  >9</button>
-<button className="btn btn-secondary col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="add"       >+</button>
-</div>
-
-<div className="row"> 
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="four"  >4</button>
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="five"  >5</button>
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="six"   >6</button>
-<button className="btn btn-secondary col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="divide"    >/</button>
-</div>
-
-<div className="row"> 
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="one"   >1</button>
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="two"   >2</button>
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="three" >3</button>
-<button className="btn btn-secondary col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="multiply"  >x</button>
-</div>
-
-<div className="row"> 
-<button className="btn btn-primary   col-md-6 col-xs-6 col-sm-6 col-lg-6 col-xl-6" onClick={show} id="zero"  >0</button>
-<button className="btn btn-primary   col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="decimal"    >.</button>
-<button className="btn btn-secondary col-md-3 col-xs-3 col-sm-3 col-lg-3 col-xl-3" onClick={show} id="subtract"  >-</button>
-</div>
 
     </div>
 </>)}
