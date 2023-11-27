3ba71a9bdcf0f899ce16929f12e748c38b5342eb
chico <paltryhumanity@gmail.com>
2023-11-26 19:07:00 -0600

    Clear lead!

--- React/clock_25_5/App.jsx
+++ React/clock_25_5/App.jsx
@@ -28,10 +28,10 @@ function Timer({t}){
 
     return (<>
         <div id="timer-label">{t.timerLabel}</div>
-        <div id="time-left">{t.sessionTime}</div>
+        <div id="time-left">{t.displayTime}</div>
         <button id="start_stop"  onClick={()=>t.play_pause(t)}><i className={ t.playButton }></i></button>
         <button id="reset" ><i className="bi bi-arrow-clockwise"></i></button>
-        <audio id="beep"/>
+        <audio ref={t.alarm} src="https://upload.wikimedia.org/wikipedia/commons/4/42/Beep_alarm_clock.ogg" id="beep"></audio>
         </>)
 }
 
@@ -43,62 +43,74 @@ function App() {
 function upwards(p){
     if (p.count == 60) return
     p.setter(p.count + 1)
+    p.setDisplay(`${ p.count +1 }:00`)
 }
 
 function downwards(p){
     if (p.count ==1) return
     p.setter(p.count - 1)
+    p.setDisplay(`${ p.count -1 }:00`)
 }
 
-function timer_down(t){
+function timer_session(t){
     // Initialize a Date object at 00:00 AM
     const startDate = new Date();
-    startDate.setHours(0, t.breakTime, 0, 0);
-
+    startDate.setHours(0, t.sessionTime, 0, 0);
+    t.setTimerLabel("Session")
     function countdown() {
         const time = {mins:startDate.getMinutes().toString().padStart(2,'0'), secs:startDate.getSeconds().toString().padStart(2,'0')}
         startDate.setSeconds(startDate.getSeconds() - 1);
-        // console.log(startDate);//normal countdown. full date displayed
         const output = `${time.mins}:${time.secs}`
         console.log(output)
-        if (output == "00:00"){ console.log("yeahh!!!"); clearInterval(tmr) }
+        t.setDisplay(output)
+        // if (!t.isPlaying) console.log("Stop!")
+        
+        if (output == "00:00"){ console.log("yeahh!!!"); clearInterval(session_countdown); alarm.current.play(); timer_break(t)}
 
     }
 
     // Use setInterval to call the countdown function every 1000 milliseconds (1 second)
-    const tmr = setInterval(countdown, 1000);
+    const session_countdown = setInterval(countdown, 1000);
 
+}
+
+function timer_break(t){
+    // Initialize a Date object at 00:00 AM
+    const startDate = new Date();
+    startDate.setHours(0, t.breakTime, 0, 0);
+    t.setTimerLabel("Break")
+    function countdown() {
+        const time = {mins:startDate.getMinutes().toString().padStart(2,'0'), secs:startDate.getSeconds().toString().padStart(2,'0')}
+        startDate.setSeconds(startDate.getSeconds() - 1);
+        const output = `${time.mins}:${time.secs}`
+        console.log(output)
+        t.setDisplay(output)
+
+
+        if (output == "00:00"){ console.log("TIME IS UP!!!"); clearInterval(break_countdown); alarm.current.play(); timer_session(t)}
+    }
+
+    // Use setInterval to call the countdown function every 1000 milliseconds (1 second)
+    const break_countdown = setInterval(countdown, 1000);
 }
 
 
 
 function play_pause(t){
-
-    timer_down(t)
-
-    // const m = moment()
-    // const _break   = m.add(t.breakTime, 'minutes').format('mm:ss') //change integer by break/session delay
-
-    // function myTimer(mom, goal) {
-    //     console.log(mom.subtract(1, 'seconds').format('mm:ss'));
-    //     if (goal==mom.format('mm:ss')) {
-    //         console.log("heyyyy!")
-    //         clearInterval(myvar)
-    //     }
-
-    // }
-
-    // let myvar = setInterval(myTimer, 1000, m, _break);
+    console.log(t.isPlaying)
 
     if ( !t.isPlaying ) {
         t.setPlayButton("bi bi-stop-circle") 
         // t.setTimerLabel("Break")
         t.setIsPlaying(true)
         //setting timer
-        setTimeout(()=>{
-            // console.log(`${ now_str } && ${break_}`)
-            console.log("Hey!")
-        }, 1000)
+        // setTimeout(()=>{
+        //     // console.log(`${ now_str } && ${break_}`)
+        //     console.log("Hey!")
+        // }, 1000)
+        
+        timer_session(t)
+
     }
 
     if ( t.isPlaying ) {
@@ -108,15 +120,21 @@ function play_pause(t){
     }
 }
 
-const [breakTime, setBreak] = useState(5)
-const [sessionTime, setSession] = useState(25)
-const bre={title:"Break Length", label:"break-label", up:"break-increment", down:"break-decrement", len:"break-length",           count:breakTime, upwards:upwards, downwards:downwards, setter:setBreak}
-const ses={title:"Session Length", label:"session-label", up:"session-increment", down:"session-decrement", len:"session-length", count:sessionTime, upwards:upwards, downwards:downwards, setter:setSession}
+// const [breakTime, setBreak] = useState(5)
+const [breakTime, setBreak] = useState(1) //for testing
+// const [sessionTime, setSession] = useState(25)
+const [sessionTime, setSession] = useState(1) //for testing
+//
+const [displayTime, setDisplay] = useState("25:00") //for testing
+//
+const alarm = useRef()
+const bre={title:"Break Length", label:"break-label", up:"break-increment", down:"break-decrement", len:"break-length",           count:breakTime, upwards:upwards, downwards:downwards, setter:setBreak, setDisplay:setDisplay, displayTime:displayTime}
+const ses={title:"Session Length", label:"session-label", up:"session-increment", down:"session-decrement", len:"session-length", count:sessionTime, upwards:upwards, downwards:downwards, setter:setSession, setDisplay:setDisplay, displayTime:displayTime}
 
 const [playButton, setPlayButton] = useState("bi bi-play-fill")
 const [timerLabel, setTimerLabel] = useState("Session")
 const [isPlaying, setIsPlaying]   = useState(false)
-const tim={isPlaying:isPlaying, setIsPlaying:setIsPlaying, label:"Session", playButton:playButton, setPlayButton:setPlayButton, timerLabel:timerLabel, setTimerLabel:setTimerLabel, play_pause:play_pause, sessionTime:sessionTime, breakTime:breakTime}
+const tim={isPlaying:isPlaying, setIsPlaying:setIsPlaying, label:"Session", playButton:playButton, setPlayButton:setPlayButton, timerLabel:timerLabel, setTimerLabel:setTimerLabel, play_pause:play_pause, sessionTime:sessionTime, breakTime:breakTime, setDisplay:setDisplay, displayTime:displayTime, alarm:alarm}
 
 return (<>
 <Buttons p={bre}/>
