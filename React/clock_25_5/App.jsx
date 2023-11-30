972732c1fc4026d4bb64def51cd81394ff0dfb34
chico <paltryhumanity@gmail.com>
2023-11-29 23:54:34 -0600

    close...

--- React/clock_25_5/App.jsx
+++ React/clock_25_5/App.jsx
@@ -29,6 +29,7 @@ function Timer({t}){
     return (<>
         <div id="timer-label">{t.timerLabel}</div>
         <div id="time-left">{t.displayTime}</div>
+        <button ref={t.timeleft} id="testingout">testing</button>
         <button id="start_stop"  onClick={()=>t.play_pause(t)}><i className={ t.playButton }></i></button>
         <button id="reset" ><i className="bi bi-arrow-clockwise"></i></button>
         <audio ref={t.alarm} src="https://upload.wikimedia.org/wikipedia/commons/4/42/Beep_alarm_clock.ogg" id="beep"></audio>
@@ -40,83 +41,39 @@ function Timer({t}){
 
 function App() {
 
+// +1 to counter
 function upwards(p){
     if (p.count == 60) return
     p.setter(p.count + 1)
     p.setDisplay(`${ p.count +1 }:00`)
+
+
+    let today = new Date()
+    startDate.setHours(0,p.count+1, 0,0)
+    // setStartDate(today)
+
+    console.log(startDate)
 }
 
+// -1 to counter 
 function downwards(p){
     if (p.count ==1) return
     p.setter(p.count - 1)
     p.setDisplay(`${ p.count -1 }:00`)
 }
 
-function timer_session(t){
-    // Initialize a Date object at 00:00 AM
-    const startDate = new Date();
-    startDate.setHours(0, t.sessionTime, 0, 0);
-    t.setTimerLabel("Session")
-    function countdown() {
-        const time = {mins:startDate.getMinutes().toString().padStart(2,'0'), secs:startDate.getSeconds().toString().padStart(2,'0')}
-        startDate.setSeconds(startDate.getSeconds() - 1);
-        const output = `${time.mins}:${time.secs}`
-        console.log(output)
-        t.setDisplay(output)
-        // if (!t.isPlaying) console.log("Stop!")
-        
-        if (output == "00:00"){ console.log("yeahh!!!"); clearInterval(session_countdown); alarm.current.play(); timer_break(t)}
-
-    }
-
-    // Use setInterval to call the countdown function every 1000 milliseconds (1 second)
-    const session_countdown = setInterval(countdown, 1000);
-
-}
-
-function timer_break(t){
-    // Initialize a Date object at 00:00 AM
-    const startDate = new Date();
-    startDate.setHours(0, t.breakTime, 0, 0);
-    t.setTimerLabel("Break")
-    function countdown() {
-        const time = {mins:startDate.getMinutes().toString().padStart(2,'0'), secs:startDate.getSeconds().toString().padStart(2,'0')}
-        startDate.setSeconds(startDate.getSeconds() - 1);
-        const output = `${time.mins}:${time.secs}`
-        console.log(output)
-        t.setDisplay(output)
-
-
-        if (output == "00:00"){ console.log("TIME IS UP!!!"); clearInterval(break_countdown); alarm.current.play(); timer_session(t)}
-    }
-
-    // Use setInterval to call the countdown function every 1000 milliseconds (1 second)
-    const break_countdown = setInterval(countdown, 1000);
-}
-
-
-
 function play_pause(t){
     console.log(t.isPlaying)
 
     if ( !t.isPlaying ) {
         t.setPlayButton("bi bi-stop-circle") 
-        // t.setTimerLabel("Break")
         t.setIsPlaying(true)
-        //setting timer
-        // setTimeout(()=>{
-        //     // console.log(`${ now_str } && ${break_}`)
-        //     console.log("Hey!")
-        // }, 1000)
-        
-        timer_session(t)
-
     }
 
     if ( t.isPlaying ) {
         t.setPlayButton("bi bi-play-fill") 
-        // t.setTimerLabel("Session")
         t.setIsPlaying(false)
+
     }
 }
 
@@ -128,30 +85,155 @@ const [sessionTime, setSession] = useState(1) //for testing
 const [displayTime, setDisplay] = useState("25:00") //for testing
 //
 const alarm = useRef()
+const timeleft = useRef()
 const bre={title:"Break Length", label:"break-label", up:"break-increment", down:"break-decrement", len:"break-length",           count:breakTime, upwards:upwards, downwards:downwards, setter:setBreak, setDisplay:setDisplay, displayTime:displayTime}
 const ses={title:"Session Length", label:"session-label", up:"session-increment", down:"session-decrement", len:"session-length", count:sessionTime, upwards:upwards, downwards:downwards, setter:setSession, setDisplay:setDisplay, displayTime:displayTime}
 
 const [playButton, setPlayButton] = useState("bi bi-play-fill")
 const [timerLabel, setTimerLabel] = useState("Session")
 const [isPlaying, setIsPlaying]   = useState(false)
-const tim={isPlaying:isPlaying, setIsPlaying:setIsPlaying, label:"Session", playButton:playButton, setPlayButton:setPlayButton, timerLabel:timerLabel, setTimerLabel:setTimerLabel, play_pause:play_pause, sessionTime:sessionTime, breakTime:breakTime, setDisplay:setDisplay, displayTime:displayTime, alarm:alarm}
+
+let session_countdown;
+let break_countdown;
+const tim={isPlaying:isPlaying, setIsPlaying:setIsPlaying, label:"Session", playButton:playButton, setPlayButton:setPlayButton, timerLabel:timerLabel, setTimerLabel:setTimerLabel, play_pause:play_pause, sessionTime:sessionTime, breakTime:breakTime, setDisplay:setDisplay, displayTime:displayTime, alarm:alarm, break_countdown:break_countdown, session_countdown:session_countdown, timeleft:timeleft}
+
+let onoff = false;
+let output;
+function today(){
+    const today = new Date();
+    today.setHours(0, sessionTime, 0, 0);
+    return today
+}
+const [startDate, setStartDate]= useState( today() );
+// const [stored, setStored]= useState( new Date() );
+useEffect(()=>{
+
+    let session_countdown;
+    let break_countdown;
+
+    timeleft.current.addEventListener("click", ()=>{
+    onoff=!onoff;
+    console.log(onoff)
+
+    function timer_session(){
+        setTimerLabel("Session")
+
+        function countDownSession() {
+            if (!onoff) { console.log("Session Stopped!!"); clearing(); }
+
+            startDate.setSeconds(startDate.getSeconds() - 1);
+            output = `${startDate.getMinutes().toString().padStart(2,'0')}:${startDate.getSeconds().toString().padStart(2,'0')}`
+            setDisplay(output)
+            console.log(output + "ongoing session") 
+
+            
+            if (output == "00:00"){ console.log("yeahh!!!"); clearInterval(session_countdown); session_countdown=null; alarm.current.play()
+               if (!break_countdown) {
+                   startDate.setHours(0,breakTime,0,0)
+                   timer_break();
+               };
+            }; 
+        };
+
+        // Use setInterval to call the countdown function every 1000 milliseconds (1 second)
+        if (!session_countdown) session_countdown = setInterval(countDownSession, 1000);
+
+    }
+
+
+
+    function timer_break(){
+        setTimerLabel("Break")
+
+        function countDownBreak() {
+            if (!onoff) { console.log("Break Stopped!!"); clearing(); }
+
+            startDate.setSeconds(startDate.getSeconds() - 1);
+            output = `${startDate.getMinutes().toString().padStart(2,'0')}:${startDate.getSeconds().toString().padStart(2,'0')}`
+            setDisplay(output)
+            console.log(output + "inbreak") 
+            
+            if (output == "00:00"){ console.log("yeahh!!!"); clearInterval(break_countdown); break_countdown=null; alarm.current.play()
+                if (!session_countdown) { 
+                    startDate.setHours(0,sessionTime,0,0)
+                    timer_session();
+                }                
+            }; 
+        };
+
+        // Use setInterval to call the countdown function every 1000 milliseconds (1 second)
+        if (!break_countdown) break_countdown = setInterval(countDownBreak, 1000);
+
+    }
+
+
+
+    function clearing (){
+        clearInterval(session_countdown)
+        session_countdown=null
+        clearInterval(break_countdown)
+        break_countdown=null
+        console.log("Stop!")
+    }
+
+
+    // if (timerLabel == "Session"){
+    //     console.log("is now on session")
+    //     timer_session()
+    // }
+
+
+    // if (timerLabel == "Break"){
+    //     console.log("is now on break")
+    //     timer_break()
+    // }
+
+
+
+    if (timerLabel == "Session"){
+        clearing()
+        console.log("is now on session")
+        if(!session_countdown) {timer_session()} 
+        if(!onoff) {clearing()}
+    }
+
+
+    if (timerLabel == "Break"){
+        clearing()
+        console.log("is now on break")
+        if(!break_countdown) {timer_break()} 
+        if(!onoff) {clearing()}
+    }
+        //
+        //
+        //
+
+    // if (timerLabel == "Session" && !session_countdown){timer_session()}
+
+    // if (timerLabel == "Break" && break_countdown){clearing_break()}
+    // if (timerLabel == "Break" && !break_countdown){timer_break()} 
+
+    // if (timerLabel == "Session" && !session_countdown){ timer_session() } else {clearing_session()}
+    // if (timerLabel == "Break" && !break_countdown){ timer_break() } else {clearing_break()}
+
+    // if (!session_countdown) { timer_session() } else {clearing_session()}
+
+    // if (!session_countdown && break_countdown) { clearing_break()}
+
+
+    })
+
+
+
+
+
+}, [startDate, timerLabel, onoff])
+
 
 return (<>
 <Buttons p={bre}/>
 <Buttons p={ses}/>
 <Timer   t={tim}/>
 
-
-
-
-
-
     </>)}
-// <div id="timer-label">session</div>
-// <div id="time-left">25:00</div>
-// <button id="start_stop"> </button>
-// <button id="reset">      </button>
-// <audio id="beep"/>
-
-
 createRoot(document.getElementById("root")).render(<App />);
