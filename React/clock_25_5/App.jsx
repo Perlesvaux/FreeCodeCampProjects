a5bf8950b3be28c94e5a1ad19e2ab58e36e73341
chico <paltryhumanity@gmail.com>
2023-12-03 18:05:02 -0600

    ALL SET!

--- React/clock_25_5/App.jsx
+++ React/clock_25_5/App.jsx
@@ -10,150 +10,218 @@ import React, {
 } from "https://esm.sh/react?dev";
 import { createRoot } from "https://esm.sh/react-dom/client?dev";
 
+function Buttons({ props }) {
+   const { state, setState, minIs60Ref, segment } = props
 
-function App(){
-    //later change to -> break:5, session:25
-    const [status, setStatus] = useState({
-        break:1,
-        session:1,
-        currentSegment: 'Session',
-        break_date: new Date('2023-11-26T00:05:00'),
-        session_date: new Date('2023-11-26T00:25:00'),
-        displayTime:"25:00",
-        isPlaying:false
-    })
+   // Get the minutes corresponding to this component:
+   let mins = segment === 'break'
+      ? state.breakTime.getMinutes()
+      : state.sessionTime.getMinutes()
 
-    let sessionInt = null
+   // If the minutes of this component reach 0, that should be displayed as 60:
+   if (mins === 0)
+   {
+      mins = 60
+   }
 
-    const alarm = useRef()
-    const start_stop = useRef()
+   const upwards = () => {
+      // If the timer is running or the minutes of this component is 60, do nothing:
+      if (state.timerIsRunning || mins === 60) return
 
+      // Create a shallow copy of state called newState. It will be the same as state except for the Date I will increment (I will increment either breakTime or sessionTime).
 
-    function upwards_break(){
-        if (status.break == 60) return
-        const auxdate = new Date( status.break_date.setHours(0,status.break+1,0,0) )
-        const currentState = {  ...status , break:status.break+1, break_date:auxdate }
-        setStatus(currentState)
-        console.log(status.break_date)
-    }
+      // When incrementing breakTime, I assign to it a new Date that is a copy of the previous breakTime, and then mutate the new breakTime before doing setState(newState).
 
-    function downwards_break(){
-        if (status.break == 1) return
-        const auxdate = new Date( status.break_date.setHours(0,status.break-1,0,0) )
-        const currentState =  { ...status , break:status.break-1, break_date:auxdate }
-        setStatus(currentState)
-        console.log(status.break_date)
-    }
+      // If I didn't assign a new Date to breakTime, the previous breakTime would be passed by reference, and then it would be mutated when I increment it later. However, React needs you to treat state as immutable. Once anything is part of state, it should not be manually mutated. The same caution applies when incrementing sessionTime.
 
-    function upwards_session(){
-        if (status.session == 60) return
-        const auxdate = new Date( status.session_date.setHours(0,status.session+1,0,0) )
-        const currentState = {  ...status , session:status.session+1, session_date:auxdate, displayTime:`${auxdate.toUTCString().slice(20,25)}`}
-        setStatus(currentState)
-        console.log(status.session_date)
-    }
+      const newState = {
+         ...state,
+         [`${segment}Time`]: new Date(state[`${segment}Time`])
+      }
 
-    function downwards_session(){
-        if (status.session == 1) return
-        const auxdate = new Date( status.session_date.setHours(0,status.session-1,0,0) )
-        const currentState = {  ...status , session:status.session-1, session_date:auxdate,  displayTime:`${auxdate.toUTCString().slice(20,25)}`}
-        setStatus(currentState)
-        console.log(status.session_date)
-    }
+      newState[`${segment}Time`].setMinutes(newState[`${segment}Time`].getMinutes() + 1)
 
-    function reset_button(){
-        const currentState = {
-            break:5,
-            session:25,
-            currentSegment: 'Session',
-            break_date: new Date('2023-11-26T00:05:00'),
-            session_date: new Date('2023-11-26T00:25:00'),
-            displayTime:"25:00",
-            isPlaying:false
-        }
+      // If this component's segment is the current segment, increase the minutes in the timer too:
+      if (state.currentSegment === segment)
+      {
+         newState.timer = new Date(newState[`${segment}Time`])
 
-        clearInterval(sessionInt)
-        sessionInt=null
+         // If the timer has been incremented to 0, the Timer component should display 60:
+         if (newState.timer.getMinutes() === 0)
+         {
+            minIs60Ref.current = true
+         }
+      }
 
-        setStatus(currentState)
-        console.log(status)
-    }
+      setState(newState)
+   }
 
+   const downwards = () => {
+      if (state.timerIsRunning || mins === 1) return
 
-    function session_countdown(){
+      minIs60Ref.current = false
 
-        const auxdate = new Date(status.session_date.setSeconds(status.session_date.getSeconds() - 1));
-        const newstate = { ...status,
-               session_date: auxdate,
-               displayTime : `${auxdate.getMinutes().toString().padStart(2,'0')}:${auxdate.getSeconds().toString().padStart(2,'0')}`
-        }
+      // COMPLETE THIS FUNCTION FOLLOWING THE EXAMPLE OF upwards:
+      
+      const newState = {
+         ...state,
+         [`${segment}Time`]: new Date(state[`${segment}Time`])
+      }
 
-            console.log(newstate.displayTime)
+      newState[`${segment}Time`].setMinutes(newState[`${segment}Time`].getMinutes() - 1)
+
+      // If this component's segment is the current segment, increase the minutes in the timer too:
+      if (state.currentSegment === segment)
+      {
+         newState.timer = new Date(newState[`${segment}Time`])
+
+         // If the timer has been incremented to 0, the Timer component should display 60:
+         if (newState.timer.getMinutes() === 0)
+         {
+            minIs60Ref.current = true
+         }
+      }
+
+      setState(newState)
 
 
-        setStatus(newstate)
-        if (newstate.displayTime == "00:00"){alarm.current.play();clearInterval(sessionInt);sessionInt=null }
-    }
+   }
 
-
-    function pause(){
-        clearInterval(sessionInt)
-        sessionInt=null
-    }
-
-    function set_on_off(){
-        setStatus({...status, isPlaying:!status.isPlaying});
-        play_pause()
-    }
-
-    function play_pause(){
-        // console.log(status.isPlaying)
-        if (status.isPlaying){
-            // setStatus({...status, isPlaying:status.isPlaying })
-            if (!sessionInt) sessionInt=setInterval(session_countdown, 1000)
-        } else {
-            pause()
-        }
-    }
-
-
-
-
-
-    useEffect(()=>{
-    // if (!status.IsPlaying) return
-    console.log(status.isPlaying)
-
-    // start_stop.current.addEventListener('click', set_on_off) 
-
-    return ()=>{
-
-    pause()
-    // start_stop.current.removeEventListener("click", set_on_off) 
-    
-    }
-
-    }, [setStatus, status ])
-
-
-    return (<>
-
-    <div     id="break-label"                                      >Break Length</div>
-    <button  id="break-increment"   onClick={upwards_break  }      ><i className="bi bi-caret-up"></i></button> 
-    <button  id="break-decrement"   onClick={downwards_break}      ><i className="bi bi-caret-down-fill"></i></button>
-    <div     id="break-length"                                     >{status.break}</div> 
-
-    <div     id="session-label"                                    >Session Length</div>
-    <button  id="session-increment" onClick={upwards_session  }    ><i className="bi bi-caret-up"></i></button> 
-    <button  id="session-decrement" onClick={downwards_session}    ><i className="bi bi-caret-down-fill"></i></button>
-    <div     id="session-length"                                   >{status.session}</div> 
-
-    <div id="timer-label">{status.currentSegment}</div>
-    <div id="time-left">{status.displayTime}</div>
-    <button id="start_stop" ref={start_stop} onClick={set_on_off} ><i className={status.isPlaying? "bi bi-stop-fill":"bi bi-play-fill" }></i></button>
-    <button id="reset" onClick={reset_button} ><i className="bi bi-arrow-clockwise"></i></button>
-    <audio ref={alarm} src="https://upload.wikimedia.org/wikipedia/commons/4/42/Beep_alarm_clock.ogg" id="beep"></audio>
-        </>)
+   return (
+      <>
+         <div id={`${segment}-label`}>{segment} Length</div>
+         <button type='button' id={`${segment}-increment`} onClick={upwards}>
+            <i className="bi bi-caret-up"></i>
+         </button>
+         <button type='button' id={`${segment}-decrement`} onClick={downwards}>
+            <i className="bi bi-caret-down-fill"></i>
+         </button>
+         <div id={`${segment}-length`}>{mins}</div>
+      </>
+   )
 }
+
+
+function Timer({ props }) {
+   const { state, setState, minIs60Ref } = props
+
+   const alarmRef = useRef(null)
+
+   const time = minIs60Ref.current
+      ? '60:00'
+      : state.timer.toTimeString().slice(3, 8)
+
+   const [playButton, setPlayButton] = useState("bi bi-play-fill")
+
+   const play_pause = () => {
+      minIs60Ref.current = false
+
+      const prevState = {...state, timerIsRunning:!state.timerIsRunning}
+      // COPY THE PREVIOUS STATE, BUT FLIP timerIsRunning TO THE OPPOSITE OF ITS CURRENT VALUE:
+
+      // CHANGE THE STATE OF THE playButton DEPENDING ON THE COPY'S timerIsRunning:
+
+      // SET THE MUTATED COPY AS THE NEW STATE:
+      setState(prevState)  
+   }
+
+   const reset = () => {
+      // PAUSE THE ALARM AND PUT IT BACK TO 0:
+       alarmRef.current.currentTime=0
+       alarmRef.current.pause()
+      
+        const currentState = {
+          breakTime: new Date('2023-11-26T00:05:00'),
+          sessionTime: new Date('2023-11-26T00:25:00'),
+          currentSegment: 'session',
+          timer: new Date('2023-11-26T00:25:00'),
+          timerIsRunning: false
+        }
+
+      // SET A NEW STATE WITH EVERYTHING RESET:
+      setState(currentState)  
+   }
+
+   useEffect(() => {
+      if (!state.timerIsRunning) return
+
+      const timeout = setTimeout(() => {
+         // IF THE TIMER IS '00:00' FROM THE PREVIOUS RENDER, COPY THE PREVIOUS STATE BUT 
+        // CHANGE THE CURRENT SEGMENT AND THE TIMER TO REFLECT THE NEW RUNNING SEGMENT, AND AFTER THAT, SET THE COPY AS THE NEW STATE, AND RETURN:
+         let currentTimer = `${state.timer.getMinutes().toString().padStart(2,'0')}:${state.timer.getSeconds().toString().padStart(2,'0')}`
+          
+          if (currentTimer === "00:00") {
+
+             let tempState = {
+                 ...state, 
+                 currentSegment: (state.currentSegment==="session")? "break":"session",
+                 timer: (state.currentSegment==="session")? new Date(state.breakTime) : new Date( state.sessionTime ),
+             }
+              // tempState.currentSegment= 'break'
+              setState(tempState)
+              return
+          }
+
+         // (THIS IS OUTSIDE OF THE ABOVE IF-BLOCK) COPY STATE BUT GIVE THE timer A NEW DATE OBJECT BASED ON THE PREVIOUS timer, AND THEN SET THE TIMER TO 1 SECOND LESS:
+         const tempState = {...state}
+         const nextDate = new Date(tempState.timer)
+         nextDate.setSeconds(nextDate.getSeconds() - 1);
+         tempState.timer = nextDate
+         currentTimer = `${tempState.timer.getMinutes().toString().padStart(2,'0')}:${tempState.timer.getSeconds().toString().padStart(2,'0')}`
+
+         // AFTER SUBTRACTING THAT 1 SECOND, IF THE NEW timer HAS REACHED '00:00' TRIGGER THE ALARM:
+         if (currentTimer == '00:00') alarmRef.current.play()  
+
+         // SET THE COPY AS THE NEW STATE:
+         setState(tempState)
+
+      }, 1000)
+      // RETURN A FUNCTION TO CLEAR THE TIMER:
+
+       return ()=>{
+           clearTimeout(timeout)
+       }
+
+   })
+
+   return (
+      <>
+         <div id="timer-label">{state.currentSegment}</div>
+         <div id="time-left">{time}</div>
+         <button id="start_stop" onClick={play_pause}>
+            <i className={playButton}></i>
+         </button>
+         <button id="reset" onClick={reset}>
+            <i className="bi bi-arrow-clockwise"></i>
+         </button>
+         <audio
+            ref={alarmRef}
+            src="https://upload.wikimedia.org/wikipedia/commons/4/42/Beep_alarm_clock.ogg" id="beep"
+         >
+         </audio>
+      </>
+   )
+}
+
+function App() {
+   const [state, setState] = useState({
+      breakTime: new Date('2023-11-26T00:05:00'),
+      sessionTime: new Date('2023-11-26T00:25:00'),
+      currentSegment: 'session',
+      timer: new Date('2023-11-26T00:25:00'),
+      timerIsRunning: false
+   })
+
+   const minIs60Ref = useRef(false)
+
+   return (
+      <>
+         <Buttons props={{ state, setState, minIs60Ref, segment: 'break' }} />
+         <Buttons props={{ state, setState, minIs60Ref, segment: 'session' }} />
+         <Timer props={{ state, setState, minIs60Ref }} />
+      </>
+   )
+}
+
 
 createRoot(document.getElementById("root")).render(<App />);
